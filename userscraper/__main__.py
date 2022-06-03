5bc28da5223463c2345719f3c257c3eb50103b20
Alejandro <amondragonflrs@gmail.com>
2022-06-02 20:13:42 -0500

    Added new skeleton argument

--- userscraper/__main__.py
+++ userscraper/__main__.py
@@ -11,5 +11,13 @@ def main():
     parser.add_argument("--followers", "-r", help="Scrape target's followers", action="store_true")
     parser.add_argument("--followees", "-e", help="Scrape target's followees", action="store_true")
     parser.add_argument("--not-followers", "-n", help="Scrape users not following target back", action="store_true")
-    parser.add_argument("--count", "-c", help="Amount of items to scrape", type=int)
+    parser.add_argument("--count", "-c", help="Maximum amount of items to scrape", type=int)
     args = parser.parse_args()
+
+    if args.not_followers and args.count:
+        raise SystemExit("--not-followers and --count cannot be used together, since all followers and followees need "
+                         "to be scraped in order to get accurate results.")
+
+
+if __name__ == "__main__":
+    main()
